e1b73b18e4bd2c84559cb383ff201f3e9f0a819e
Russell Mull <russell@digitalartificer.com>
2016-04-19 11:06:14 -0700

    Modest notmuch improvements

--- layers/notmuch/packages.el
+++ layers/notmuch/packages.el
@@ -1,35 +1,27 @@
 ;;; packages.el --- notmuch Layer packages File for Spacemacs
-;;
-;; Copyright (c) 2012-2014 Sylvain Benner
-;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
-;;
-;; Author: Sylvain Benner <sylvain.benner@gmail.com>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; License: GPLv3
 
 ;; List of all packages to install and/or initialize. Built-in packages
 ;; which require an initialization must be listed explicitly in the list.
 (setq notmuch-packages
-    '(
-      notmuch
+    '(notmuch
       notmuch-labeler
-      notmuch-unread
-      ))
+      notmuch-unread))
 
 ;; List of packages to exclude.
 (setq notmuch-excluded-packages '())
 
 ;; For each package, define a function notmuch/init-<package-notmuch>
-;;
 (defun notmuch/init-notmuch ()
   "Initialize my package"
   (use-package notmuch
     :defer t
     :commands notmuch
-    :init (evil-leader/set-key "an" 'notmuch)
+    :init (progn
+            (evil-leader/set-key "an" 'notmuch)
+            (evil-leader/set-key "Ns" 'notmuch-search)
+            (evil-leader/set-key "Nc" 'notmuch-mua-new-mail)
+            
+            ) 
     :config (progn
               ;; Fix helm
               ;; See id:m2vbonxkum.fsf@guru.guru-group.fi
